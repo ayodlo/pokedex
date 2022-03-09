314a4e9593c911e8afd492dee03eabad75e1aa40
Devon Lewis <dlewis@llac.org>
2022-03-08 17:39:19 -0700

    Refactoring fetch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import React from 'react';
+import AllPokemon from './components/AllPokemon';
+
 function App() {
-  return <div>Hello</div>;
+  const [state, setState] = React.useState({
+    pokemon: [],
+    status: 'loading',
+    error: null,
+  });
+
+  const { status, error, pokemon } = state;
+
+  const endpoint =
+    'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
+
+  React.useEffect(() => {
+    setState((state) => {
+      return { ...state, status: 'loading' };
+    });
+
+    fetch(endpoint)
+      .then(async (response) => {
+        const data = await response.json();
+        setState((state) => {
+          return { ...state, pokemon: data.pokemon, status: 'success' };
+        });
+      })
+      .catch((error) => {
+        setState((state) => {
+          return {
+            error,
+            status: 'error',
+          };
+        });
+      });
+
+    return () => {
+      setState({});
+    };
+  }, []);
+
+  return (
+    <>
+      {status === 'loading' ? (
+        'Loading'
+      ) : status === 'error' ? (
+        'Something Went Wrong Fetching the Data'
+      ) : (
+        <AllPokemon pokemon={pokemon} />
+      )}
+    </>
+  );
 }
 
 export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+export function client(endpoint, customConfig = {}) {
+  const config = {
+    method: 'GET',
+    ...customConfig,
+  };
+
+  return window.fetch(`${endpoint}`, config).then(async (response) => {
+    const data = await response.json();
+    if (response.ok) {
+      return data;
+    } else {
+      return Promise.reject(data);
+    }
+  });
+}
